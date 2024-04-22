aa9e5d5e6d33d43363ed92961779c0ac0d254a4f
Nattapong Unaregul <tylerlantern@gmail.com>
2024-04-22 12:38:51 +0700

    fix zooming

--- src/app/richtext/page.tsx
+++ src/app/richtext/page.tsx
@@ -2,6 +2,9 @@
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import 'froala-editor/js/plugins/image.min.js';
+import "froala-editor/js/third_party/embedly.min.js";
+import "froala-editor/js/plugins/fullscreen.min.js";
+import "froala-editor/css/plugins/fullscreen.min.css";
 import FroalaEditorComponent from 'react-froala-wysiwyg';
 
 export default function RichText() {
@@ -14,17 +17,96 @@ export default function RichText() {
       }
     },
     toolbarButtons: {
-      'moreText': {
-        'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
+      moreText: {
+        buttons: [
+          "bold",
+          "italic",
+          "underline",
+          "strikeThrough",
+          "subscript",
+          "superscript",
+          "fontFamily",
+          "fontSize",
+          "textColor",
+          "backgroundColor",
+          "inlineClass",
+          "inlineStyle",
+          "clearFormatting"
+        ]
       },
-      'moreRich': {
-        'buttons': ['insertLink', 'insertImage', 'insertVideo', 'insertTable', 'emoticons', 'fontAwesome', 'specialCharacters', 'embedly', 'insertFile', 'insertHR']
-
+      moreParagraph: {
+        buttons: [
+          "alignLeft",
+          "alignCenter",
+          "formatOLSimple",
+          "alignRight",
+          "alignJustify",
+          "formatOL",
+          "formatUL",
+          "paragraphFormat",
+          "paragraphStyle",
+          "lineHeight",
+          "outdent",
+          "indent",
+          "quote"
+        ]
       },
-      'moreMisc': {
-        'buttons': ['undo', 'redo', 'fullscreen', 'print', 'getPDF', 'spellChecker', 'selectAll', 'html', 'help'],
-        'align': 'right',
-      }
+      moreRich: {
+        buttons: [
+          "insertLink",
+          "insertImage",
+          "insertVideo",
+          "insertTable",
+          "emoticons",
+          "fontAwesome",
+          "specialCharacters",
+          "embedly",
+          "insertFile",
+          "insertHR"
+        ]
+      },
+      moreMisc: {
+        buttons: [
+          "undo",
+          "redo",
+          "fullscreen",
+          "print",
+          "getPDF",
+          "spellChecker",
+          "selectAll",
+          "html",
+          "help"
+        ],
+        align: "right",
+        buttonsVisible: 2,
+      },
+      pluginsEnabled: [
+        "table",
+        "spell",
+        "quote",
+        "save",
+        "quickInsert",
+        "paragraphFormat",
+        "paragraphStyle",
+        "help",
+        "draggable",
+        "align",
+        "link",
+        "lists",
+        "file",
+        "image",
+        "emoticons",
+        "url",
+        "video",
+        "embedly",
+        "colors",
+        "entities",
+        "inlineClass",
+        "inlineStyle",
+        // 'codeBeautif '
+        // 'spellChecker',
+        "imageTUI"
+      ]
     },
   };
   return (
