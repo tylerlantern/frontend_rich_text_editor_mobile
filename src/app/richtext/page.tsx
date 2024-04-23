602e4838dd71f32c10d8eb4d71c0ecb6f69672a1
Nattapong Unaregul <tylerlantern@gmail.com>
2024-04-23 14:41:03 +0700

    dragable

--- src/app/richtext/page.tsx
+++ src/app/richtext/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/js/plugins/draggable.min.js';
 import 'froala-editor/js/plugins/image.min.js';
 import "froala-editor/js/third_party/embedly.min.js";
 import "froala-editor/js/plugins/fullscreen.min.js";
