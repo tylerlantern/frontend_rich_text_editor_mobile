9439674b84d284d793ee08e3f756e5f9796f2327
Nattapong Unaregul <tylerlantern@gmail.com>
2024-04-24 10:36:34 +0700

    refine import

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+import CopyPlugin from 'copy-webpack-plugin';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+const nextConfig = {
+  webpack: (config) => {
+    // append the CopyPlugin to copy the file to your public dir
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: "node_modules/froala-editor", to: "../public/froala-editor" },
+        ],
+      }),
+    )
+    // Important: return the modified config
+    return config
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  }
+}
 
 export default nextConfig;
